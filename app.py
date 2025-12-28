6abc2ae81e3cd120fdebc566b6e57518ee5f48b5
itsanshika8345-tech <itsanshika8345@gmail.com>
2025-12-28 16:16:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ scaled_X = scaler.fit_transform(X)
 
 final_value = scaler.transform([all_value])
 
-model = RandomForestRegressor()
+model = st.cache(RandomForestRegressor)()
 model.fit(X,y)
 house_price = model.predict(final_value)[0]
 
@@ -39,6 +39,7 @@ msg = f'''House price is ${house_price*100000}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Anshika**''')
+
 
 
 
