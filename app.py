053b89219e3f341d08d7ab1b4f5ed89c2ada4b28
itsanshika8345-tech <itsanshika8345@gmail.com>
2025-12-28 16:33:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,10 @@ scaled_X = scaler.fit_transform(X)
 
 final_value = scaler.transform([all_value])
 
-model = st.cache(RandomForestRegressor)()
-model.fit(X,y)
+def model_run():
+  model = st.cache(RandomForestRegressor)()
+  model.fit(X,y)
+  
 house_price = model.predict(final_value)[0]
 
 with st.spinner('Predicting house price'):
@@ -39,6 +41,7 @@ msg = f'''House price is ${round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Anshika**''')
+
 
 
 
