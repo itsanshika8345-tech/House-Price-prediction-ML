8344ff6a071e127ce2ac13a62c8f6f56f31e3cf5
itsanshika8345-tech <itsanshika8345@gmail.com>
2025-12-28 16:35:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,10 +28,13 @@ scaler = StandardScaler()
 scaled_X = scaler.fit_transform(X)
 
 final_value = scaler.transform([all_value])
-
-def model_run():
-  model = st.cache(RandomForestRegressor)()
+@st.cache_data
+def model_run(X,y):
+  model = RandomForestRegressor()
   model.fit(X,y)
+  return model
+
+model = model_run(X,y)
   
 house_price = model.predict(final_value)[0]
 
@@ -41,6 +44,7 @@ msg = f'''House price is ${round(house_price*100000,2)}'''
 st.success(msg)
 
 st.markdown('''**Design and Developed by: Anshika**''')
+
 
 
 
