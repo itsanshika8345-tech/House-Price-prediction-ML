40e25b2d3bbc29c505fdee105f1de20e2fe6296e
itsanshika8345-tech <itsanshika8345@gmail.com>
2025-12-28 16:03:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,11 @@ model.fit(X,y)
 house_price = model.predict(final_value)
 
 with st.spinner('Predicting house price'):
-  time.sleep(3)
-st.write(house_price)
+  time.sleep(1)
+msg = f'''House price is ${house_price*100000}'''
+st.success(msg)
+
+st.markdown('''**Design and Developed by: Anshika**''')
 
 
 
